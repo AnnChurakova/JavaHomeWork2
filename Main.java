c6bde8de9e22e0e236ffa2c87072e69d42633ce9
AnnChurakova <frau.anya1@gmail.com>
2022-06-03 23:37:53 +0300

    1245
    1234567u8i

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+    int amount = 960;
+    int bonus = amount / 20;
+
+        System.out.println("Итого бонусных миль: " + bonus );
+    }
+}
